0bcdc9ec0cb8800c73fed4e4f2cacbefc03863b7
root <root@localhost.localdomain>
2022-08-03 11:07:55 -0500

    Subiendo aws-token-relayers

--- tools/aws-token-relayer.py
+++ tools/aws-token-relayer.py
@@ -4,6 +4,6 @@ from warrant.aws_srp import AWSSRP
 client = boto3.client('cognito-idp', region_name='us-west-2') 
 #aws = AWSSRP(username='API_KEY', password='API_SECRET', pool_id='POOL_ID', client_id='CLIENT_ID', client=client) 
 #aws = AWSSRP(username='ramses.hernandez', password='Ramses_19', pool_id='us-east-1_UXjf1ZhVi', client_id='2thrbvaer1j0gsh6nfkfc18r4b', client=client) 
-aws = AWSSRP(username='JCXA47m6YWoGTX3EkFMt9CXUNojLM14b', password='273DVM8F3SbikrhSVVWde3xbyKV9HmPQkuJSmkJbUdEDm2oS9i9ZjUFXRSFm549F', pool_id='us-west-2_94f3puJWv', client_id='40e58hbc7pktmnp9i26hh5nsav', client=client) 
+aws = AWSSRP(username='3sB9hyBa8Njb8549JXsPKFBUKUD1YAPY', password='2VVPeusPCyQ8VoJmsQUSbeXfhK7GCTnBnm74TtePcNVc7HByRiEJfskALo56Ak7B', pool_id='us-west-2_iLmIggsiy', client_id='1bpd19lcr33qvg5cr3oi79rdap', client=client) 
 tokens = aws.authenticate_user() 
 print('Access Token', tokens['AuthenticationResult']['AccessToken'])
