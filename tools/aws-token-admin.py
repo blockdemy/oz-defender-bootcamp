ccf74d5240742ba7dbfdd5c1a1b40ea6e253dd98
Ozymandias <gasram@hotmail.com>
2022-08-04 10:09:00 -0500

    Update aws-token-admin.py

--- tools/aws-token-admin.py
+++ tools/aws-token-admin.py
@@ -3,7 +3,7 @@ from warrant.aws_srp import AWSSRP
 
 client = boto3.client('cognito-idp', region_name='us-west-2') 
 #aws = AWSSRP(username='API_KEY', password='API_SECRET', pool_id='POOL_ID', client_id='CLIENT_ID', client=client) 
-#aws = AWSSRP(username='ramses.hernandez', password='Ramses_19', pool_id='us-east-1_UXjf1ZhVi', client_id='2thrbvaer1j0gsh6nfkfc18r4b', client=client) 
+#aws = AWSSRP(username='xxx', password='xxx', pool_id='us-east-1_UXjf1ZhVi', client_id='2thrbvaer1j0gsh6nfkfc18r4b', client=client) 
 aws = AWSSRP(username='JCXA47m6YWoGTX3EkFMt9CXUNojLM14b', password='273DVM8F3SbikrhSVVWde3xbyKV9HmPQkuJSmkJbUdEDm2oS9i9ZjUFXRSFm549F', pool_id='us-west-2_94f3puJWv', client_id='40e58hbc7pktmnp9i26hh5nsav', client=client) 
 tokens = aws.authenticate_user() 
 print('Access Token', tokens['AuthenticationResult']['AccessToken'])
